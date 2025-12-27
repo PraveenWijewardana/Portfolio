b66467a30b52073ad561dcc161b61f1a2864ff3e
Praveen Wijewardana <praveenwijewardana1@gmail.com>
2025-12-27 21:27:52 +0530

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+function msg(){
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let project = document.getElementById("project").value;
+    
+    
+}
